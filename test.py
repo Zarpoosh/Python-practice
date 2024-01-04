c95ba6b3615059b42fe009b0622f2720a48437da
Zarpoosh <mzar67839@gmail.com>
2024-01-04 17:38:28 +0330

    create files

---
+++ test.py
@@ -0,0 +1,51 @@
+# from html.parser import HTMLParser
+# from html.entities import name2codepoint
+
+# class MyHTMLParser(HTMLParser):
+#     def handle_starttag(self, tag, attrs):
+#         print("Start tag:", tag)
+#         for attr in attrs:
+#             print("     attr:", attr)
+
+#     def handle_endtag(self, tag):
+#         print("End tag  :", tag)
+
+#     def handle_data(self, data):
+#         print("Data     :", data)
+
+#     def handle_comment(self, data):
+#         print("Comment  :", data)
+
+#     def handle_entityref(self, name):
+#         c = chr(name2codepoint[name])
+#         print("Named ent:", c)
+
+#     def handle_charref(self, name):
+#         if name.startswith('x'):
+#             c = chr(int(name[1:], 16))
+#         else:
+#             c = chr(int(name))
+#         print("Num ent  :", c)
+
+#     def handle_decl(self, data):
+#         print("Decl     :", data)
+
+# parser = MyHTMLParser()
+
+
+
+from html.parser import HTMLParser
+
+class MyHTMLParser(HTMLParser):
+    def handle_starttag(self, tag, attrs):
+        print("Encountered a start tag:", tag)
+
+    def handle_endtag(self, tag):
+        print("Encountered an end tag :", tag)
+
+    def handle_data(self, data):
+        print("Encountered some data  :", data)
+
+parser = MyHTMLParser()
+parser.feed('<html><head><title>Test</title></head>'
+            '<body><h1>Parse me!</h1></body></html>')
