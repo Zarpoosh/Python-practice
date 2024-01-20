db626df8c9dd5b1b4f93da5c8f84d7fbc3eff65a
Zarpoosh <mzar67839@gmail.com>
2024-01-20 19:32:35 +0330

    add practical codes in python

---
+++ README.md
@@ -0,0 +1,60 @@
+# Hi there 👋 im  Minoo Zarpoosh a Front end Developer !<br>
+
+## My Skills :<br>
+<div align="center">
+<a><img alt="Static Badge" src="https://img.shields.io/badge/React%20js-ff0000"></a>
+<a><img alt="Static Badge" src="https://img.shields.io/badge/Javascript-ff9000"></a>
+<a><img alt="Static Badge" src="https://img.shields.io/badge/Material%20UI-ffff00"></a>
+<a><img alt="Static Badge" src="https://img.shields.io/badge/Responsive%20development-00ad14"></a>
+<a><img alt="Static Badge" src="https://img.shields.io/badge/Tailwind%20CSS-00d3d3"></a>
+<br>
+<a><img alt="Static Badge" src="https://img.shields.io/badge/Bootstrap%204&5-0008ff"></a>
+<a><img alt="Static Badge" src="https://img.shields.io/badge/HTML5-6100ff"></a>
+<a><img alt="Static Badge" src="https://img.shields.io/badge/CSS3-fa00ff"></a>
+<a><img alt="Static Badge" src="https://img.shields.io/badge/Git-00ff90"></a>
+<a><img alt="Static Badge" src="https://img.shields.io/badge/Linux (Ubuntu)-ff009d"></a>
+</div>
+
+<br><br>
+
+## Intrested:<br>
+1- <a><img alt="Static Badge" src="https://img.shields.io/badge/Coding-ff0000"></a><br>
+2- <a><img alt="Static Badge" src="https://img.shields.io/badge/Trading%20in%20Forex-ff9000"></a><br>
+3- <a><img alt="Static Badge" src="https://img.shields.io/badge/Ai-ffff00"></a><br>
+4- <a><img alt="Static Badge" src="https://img.shields.io/badge/Play%20Piano-00ad14"></a><br>
+5- <a><img alt="Static Badge" src="https://img.shields.io/badge/Sing-00d3d3"></a>
+
+
+
+
+
+<!-- 
+**Zarpoosh/Zarpoosh** is a ✨ _special_ ✨ repository because its `README.md` (this file) appears on your GitHub profile.
+
+Here are some ideas to get you started:
+
+- 🔭 I’m currently working on ...
+- 🌱 I’m currently learning ...
+- 👯 I’m looking to collaborate on ...
+- 🤔 I’m looking for help with ...
+- 💬 Ask me about ...
+- 📫 How to reach me: ...
+- 😄 Pronouns: ...
+- ⚡ Fun fact: ...
+
+
+> [!NOTE]
+> Useful information that users should know, even when skimming content.
+
+> [!TIP]
+> Helpful advice for doing things better or more easily.
+
+> [!IMPORTANT]
+> Key information users need to know to achieve their goal.
+
+> [!WARNING]
+> Urgent info that needs immediate user attention to avoid problems.
+
+> [!CAUTION]
+> Advises about risks or negative outcomes of certain actions.
+ -->
--- test.py
+++ test.py
@@ -34,18 +34,117 @@
 
 
 
-from html.parser import HTMLParser
+# def f(**a):
+#     print(a)
+#     return a["tool"]*a["arz"]
 
-class MyHTMLParser(HTMLParser):
-    def handle_starttag(self, tag, attrs):
-        print("Encountered a start tag:", tag)
+# x=print(arz=25)
 
-    def handle_endtag(self, tag):
-        print("Encountered an end tag :", tag)
+# !---------------------
 
-    def handle_data(self, data):
-        print("Encountered some data  :", data)
+# list=[]
+# temp=[]
+# for i in range(3):
+#     for j in range(2):
+#         a= int(input(":"))
+#         temp.append(a)
+#     list.append(temp)
+#     print(list)
 
-parser = MyHTMLParser()
-parser.feed('<html><head><title>Test</title></head>'
-            '<body><h1>Parse me!</h1></body></html>')
+# !--------------------------
+# def sum(*a):
+#     sum=0
+#     for item in a:
+#      sum+=item
+#     return sum
+
+# s=sum(2,3,4,5,6)
+# print(s)
+
+# !-----------------------
+# a=[2,3,4]
+# b=["w", "x", "y", "z"]
+# m=zip(a,b)
+# print(type(m))
+# n=list(m)
+# print(n)
+# !----------------------
+# ch=input(":")
+# if ch>="A" and ch<="Z":
+#     print("UPPER")
+# else:
+#     print("lower")
+
+# !-------------------- wrong -------------
+# ch=input(":")
+# if ch>=65 and ch<=90:
+#     print("UPPER")
+# else:
+#     print("lower")
+    
+# !---------------------
+# chr=input("enter a char:")
+# print(f"{chr}:{ord(chr)}")
+
+# !------------------------
+# x=int(input("enter number :"))
+# a="hello" if x>5 else "bye"
+# print(a)
+#!----------------------------
+# a=[1]+[2]*2+[3]*3
+# print(a)
+# x,*y,z,r,t,m=a
+# print(y*5)
+# ! --------------------------
+# x=[2,3,4,5,6]
+# del x[1:3]
+# print(x)
+# !--------------------------
+# x=[2,3,4,5]
+# a,*b,c=x
+# print(a,b,c)
+# !---------------------------
+# x=[2,3,4,5]
+# i=x.index(3)
+# print(i)
+# !--------------------------
+# ch=list("Minoo \n Zarpoosh")
+# print(ch)
+# for i in ch:
+#     print(i, end=" ")
+# !--------------------------
+
+# a = list(map(int, input("enter 4 numbers : ").split()))
+
+# min_diff = abs(a[0] - a[1])
+# min_pair = [a[0], a[1]]
+
+# for i in range(2, len(a)):
+#   for j in range(i + 1, len(a)):
+#     if abs(a[i] - a[j]) < min_diff:
+#       min_diff = abs(a[i] - a[j])
+#       min_pair = [a[i], a[j]]
+
+# print("2 numbers with min abs:", min_pair)
+# !--------------------------
+# x=range(0,10,2)
+# print(x)
+# y=list(x)
+# print(y)
+# !--------------------------
+# a=[2,3]
+# b=[4,7]
+# c=a+b
+# print(c)
+# !--------------------------
+phone_numbers={"minoo":"09940" ,"maryam":"7566354", "kimia":"656421" , "dady":"9954648"}
+k=input("Enter name:")
+if k in phone_numbers:
+    print(phone_numbers[k])
+else:
+    print("404 not found!")
+    answer=input("do you want add to note book?(y/n)")
+    if answer=="y":
+        new_number=input(f"what is {k}'s number ?")
+        phone_numbers[k]=new_number
+print(phone_numbers)
